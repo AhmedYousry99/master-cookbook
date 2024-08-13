77b04fc1931606b9ba70f2fc93aefe61645f2cc9
Khaatora <ahosari20@gmail.com>
2024-08-13 18:58:35 +0300

    init app

---
+++ app/src/main/java/com/senseicoder/mastercookbook/splash/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.senseicoder.mastercookbook.splash;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.senseicoder.mastercookbook.R;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+    }
+}
