1c84d8364f23a5d8028fc886fe6f091c715fed84
Khaatora <ahosari20@gmail.com>
2024-08-19 20:03:23 +0300

    added Food API keys

---
+++ app/src/main/java/com/senseicoder/mastercookbook/model/DTOs/CategoryDTO.java
@@ -0,0 +1,54 @@
+package com.senseicoder.mastercookbook.model.DTOs;
+
+import com.google.gson.annotations.SerializedName;
+
+public class CategoryDTO {
+
+    @SerializedName(MealKeys.STR_CATEGORY)
+    private String title;
+    @SerializedName(MealKeys.ID_CATEGORY)
+    private String id;
+    @SerializedName(MealKeys.STR_CATEGORY_THUMB)
+    private String thumbnail;
+    @SerializedName(MealKeys.STR_CATEGORY_DESCRIPTION)
+    private String description;
+
+    public CategoryDTO(String title, String id, String thumbnail, String description) {
+        this.title = title;
+        this.id = id;
+        this.thumbnail = thumbnail;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
