ec4e0035a9832f6937b9217e52ddd7dcc75b7931
Khaatora <ahosari20@gmail.com>
2024-08-19 20:25:18 +0300

    added ui utils class

---
+++ app/src/main/java/com/senseicoder/mastercookbook/util/global/UiUtils.java
@@ -0,0 +1,21 @@
+package com.senseicoder.mastercookbook.util.global;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class UiUtils {
+
+    public static void applyAppBarInsetsOnView(View view){
+        ViewCompat.setOnApplyWindowInsetsListener(view, (view1, insets) -> {
+            int statusBarHeight = insets.getInsets(WindowInsetsCompat.Type.statusBars()).top;
+            // Adjust the top margin of the toolbar
+            ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) view1.getLayoutParams();
+            params.topMargin = statusBarHeight;
+            view1.setLayoutParams(params);
+            return insets;
+        });
+    }
+}
