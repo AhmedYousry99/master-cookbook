ea2bfb042b6cac9a02e76ec12b8c6952af6aa38d
Khaatora <ahosari20@gmail.com>
2024-08-23 23:53:18 +0300

    moved mealkeys and added imports

--- app/src/main/java/com/senseicoder/mastercookbook/model/responses/GetMealsResponse.java
+++ app/src/main/java/com/senseicoder/mastercookbook/model/responses/GetMealsResponse.java
@@ -2,7 +2,7 @@ package com.senseicoder.mastercookbook.model.responses;
 
 import com.google.gson.annotations.SerializedName;
 import com.senseicoder.mastercookbook.model.DTOs.MealDTO;
-import com.senseicoder.mastercookbook.model.DTOs.MealKeys;
+import com.senseicoder.mastercookbook.model.MealKeys;
 
 import java.util.List;
 
