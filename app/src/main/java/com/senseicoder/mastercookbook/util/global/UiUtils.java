59735b5e351ae6f96857af04782f459bbb83ac13
Khaatora <ahosari20@gmail.com>
2024-08-23 03:17:53 +0300

    added ui functionality

--- app/src/main/java/com/senseicoder/mastercookbook/util/global/UiUtils.java
+++ app/src/main/java/com/senseicoder/mastercookbook/util/global/UiUtils.java
@@ -1,14 +1,15 @@
 package com.senseicoder.mastercookbook.util.global;
 
+import android.app.Activity;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.view.WindowManager;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
+import androidx.fragment.app.FragmentActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 import com.senseicoder.mastercookbook.R;
-
-import java.util.ArrayList;
 
 public class UiUtils {
 
@@ -21,6 +22,20 @@ public class UiUtils {
             view1.setLayoutParams(params);
             return insets;
         });
+    }
+
+    public static void preventSoftKeyboardFromPushingUpFragment(Activity activity){
+        activity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+    }
+
+
+    public static NavHostFragment getNavHostFragment(FragmentActivity fragmentActivity) {
+        return (NavHostFragment) fragmentActivity.getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_home);
+    }
+
+    public static NavController getNavController(FragmentActivity fragmentActivity){
+        NavHostFragment navHostFragment = (NavHostFragment) fragmentActivity.getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_home);
+        return navHostFragment.getNavController();
     }
 
 }
