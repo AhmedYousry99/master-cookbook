9b1dc348c9810bc300b533c4d49df59b41bb15f6
Khaatora <ahosari20@gmail.com>
2024-08-23 03:07:53 +0300

    added search functionality and finished ui

--- app/src/main/java/com/senseicoder/mastercookbook/features/main/ui/search/SearchFragment.java
+++ app/src/main/java/com/senseicoder/mastercookbook/features/main/ui/search/SearchFragment.java
@@ -4,14 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import com.senseicoder.mastercookbook.R;
-import com.senseicoder.mastercookbook.util.global.UiUtils;
+
+import com.senseicoder.mastercookbook.util.enums.SearchType;
 
 public class SearchFragment extends Fragment {
+
+    Button searchByMealButton;
+    Button searchByCategoryButton;
+    Button searchByIngredientButton;
+    Button searchByCountryButton;
+
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -20,6 +30,23 @@ public class SearchFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
+        searchByMealButton = view.findViewById(R.id.searchByMealButton);
+        searchByCategoryButton = view.findViewById(R.id.searchByCategoryButton);
+        searchByIngredientButton = view.findViewById(R.id.searchByIngredientButton);
+        searchByCountryButton = view.findViewById(R.id.searchByCountryButton);
+        searchByMealButton.setOnClickListener(v -> handleButtonClick(SearchType.Name));
+                searchByCategoryButton.setOnClickListener(v -> handleButtonClick(SearchType.Category));
+        searchByIngredientButton.setOnClickListener(v -> handleButtonClick(SearchType.Ingredient));
+                searchByCountryButton.setOnClickListener(v -> handleButtonClick(SearchType.Country));
+    }
 
+    private void handleButtonClick(SearchType searchType){
+        SearchFragmentDirections.ActionSearchFragmentToSearchDetailedFragment action =
+                SearchFragmentDirections.actionSearchFragmentToSearchDetailedFragment(searchType);
+        Navigation.findNavController(getView()).navigate(action);
+    }
 }
